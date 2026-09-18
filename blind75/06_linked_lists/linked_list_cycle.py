4124576a985b8e6d7903f5ed767833b391cf9ac2
Aaditya Edupuganti <aaditya4401@gmail.com>
2026-09-18 11:48:29 +0530

    linked list easy done

--- blind75/06_linked_lists/linked_list_cycle.py
+++ blind75/06_linked_lists/linked_list_cycle.py
@@ -36,7 +36,17 @@ from common.structures import ListNode
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        pass
+        fast = head
+        slow = head
+
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+
+            if fast is slow:
+                return True
+
+        return False
 
 
 if __name__ == "__main__":
--- blind75/06_linked_lists/reverse_linked_list.py
+++ blind75/06_linked_lists/reverse_linked_list.py
@@ -35,6 +35,15 @@ from common.structures import ListNode
 
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        node = None
+
+        while head:
+            temp = head.next
+            head.next = node
+            node = head
+            head = temp
+
+        return node
         pass
 
 
